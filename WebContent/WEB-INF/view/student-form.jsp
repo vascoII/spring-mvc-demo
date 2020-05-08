1b124fe542302c25f617b5d1c99dabfe067b6aa1
Fabrice Da Silva <f-dasilva2@autodistribution.com>
2020-05-08 14:15:02 +0200

    Added: use properties file for list

--- WebContent/WEB-INF/view/student-form.jsp
+++ WebContent/WEB-INF/view/student-form.jsp
@@ -12,9 +12,9 @@
 		<br><br>
 		Last Name: <form:input path="lastName" />
 		<br><br>
-		Country : <form:select path="country">
-			<form:options items="${student.countryOptions}" />
-		</form:select>	
+		Country : <form:select path="country"> 
+ 			<form:options items="${theCountryOptions}" />
+		</form:select>
 		<br><br>
 		<input type="submit" value="Submit">
 	</form:form>
--- src/springdemo/controller/StudentController.java
+++ src/springdemo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package springdemo.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,12 +13,17 @@ import springdemo.model.Student;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+	
+	@Value("#{countryOptions}") 
+	private Map<String, String> countryOptions;
 
 	@RequestMapping("/showForm")
 	public String showForm(Model studentModel) {
 		Student student = new Student();
-		
+		// add student object to the model 
 		studentModel.addAttribute("student", student);
+		// add the country options to the model 
+		studentModel.addAttribute("theCountryOptions", countryOptions); 
 		return "student-form";
 	}
 	
